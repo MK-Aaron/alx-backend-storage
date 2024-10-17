d576e3b405236130857137ad41b5ac734b994ced
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-10-17 17:31:20 +0300

    Typo

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- creates a table with three columns
 
-UPDATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
