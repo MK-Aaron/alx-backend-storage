96caed5de6a2a1553d5192261eb599633e00d650
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-10-17 17:22:18 +0300

    USE: ENUM string

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- creates a table with three columns
+UPDATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
