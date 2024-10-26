f52f9444339afba19c61a38b60f4aa23221037db
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-10-27 02:17:48 +0300

    Complete task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
+'''
+A module for using the Redis NoSQL data storage.
+'''
+from typing import Union
 import redis
 import uuid
-from typing import Union
-"""
-Redis class caching
-"""
 
 
 class Cache:
+    '''Represents an object for storing data in a Redis data storage.
+    '''
     def __init__(self) -> None:
-        sef._resis = redis.Redis()
-        self.redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
 
-    def store(self, daa: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def store(self, data:  Union[str, bytes, int, float]) -> str:
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
